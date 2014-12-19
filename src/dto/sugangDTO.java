16baa30ae5aa2da6b62d121b2809c8f4a66d69f9
SangMin <abc@abc.com>
2014-12-19 14:22:19 +0900

    업로드

--- src/dto/sugangDTO.java
+++ src/dto/sugangDTO.java
@@ -1,5 +1,132 @@
 package dto;
 
 public class sugangDTO {
-
+	String m_code;	
+	String l_type;	
+	String l_code;	
+	String l_name;	
+	String professor;	
+	String l_room;	
+	int y_grade;	
+	int f_grade;	
+	int semester;
+	int l_start; 	
+	int l_end;	
+	String l_day;	
+	int in_stu;	
+	int limit_stu;	
+	String s_date;	
+	String l_date;
+	
+	int count;
+	int day;
+	
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getM_code() {
+		return m_code;
+	}
+	public void setM_code(String m_code) {
+		this.m_code = m_code;
+	}
+	public String getL_type() {
+		return l_type;
+	}
+	public void setL_type(String l_type) {
+		this.l_type = l_type;
+	}
+	public String getL_code() {
+		return l_code;
+	}
+	public void setL_code(String l_code) {
+		this.l_code = l_code;
+	}
+	public String getL_name() {
+		return l_name;
+	}
+	public void setL_name(String l_name) {
+		this.l_name = l_name;
+	}
+	public String getProfessor() {
+		return professor;
+	}
+	public void setProfessor(String professor) {
+		this.professor = professor;
+	}
+	public String getL_room() {
+		return l_room;
+	}
+	public void setL_room(String l_room) {
+		this.l_room = l_room;
+	}
+	public int getY_grade() {
+		return y_grade;
+	}
+	public void setY_grade(int y_grade) {
+		this.y_grade = y_grade;
+	}
+	public int getF_grade() {
+		return f_grade;
+	}
+	public void setF_grade(int f_grade) {
+		this.f_grade = f_grade;
+	}
+	public int getSemester() {
+		return semester;
+	}
+	public void setSemester(int semester) {
+		this.semester = semester;
+	}
+	public int getL_start() {
+		return l_start;
+	}
+	public void setL_start(int l_start) {
+		this.l_start = l_start;
+	}
+	public int getL_end() {
+		return l_end;
+	}
+	public void setL_end(int l_end) {
+		this.l_end = l_end;
+	}
+	public String getL_day() {
+		return l_day;
+	}
+	public void setL_day(String l_day) {
+		this.l_day = l_day;
+	}
+	public int getIn_stu() {
+		return in_stu;
+	}
+	public void setIn_stu(int in_stu) {
+		this.in_stu = in_stu;
+	}
+	public int getLimit_stu() {
+		return limit_stu;
+	}
+	public void setLimit_stu(int limit_stu) {
+		this.limit_stu = limit_stu;
+	}
+	public String getS_date() {
+		return s_date;
+	}
+	public void setS_date(String s_date) {
+		this.s_date = s_date;
+	}
+	public String getL_date() {
+		return l_date;
+	}
+	public void setL_date(String l_date) {
+		this.l_date = l_date;
+	}	
 }
